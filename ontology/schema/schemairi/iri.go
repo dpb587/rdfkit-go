33af408fddc5eb3347e4550eb165068ddeb33336
Danny Berger <dpb587@gmail.com>
2025-05-30 16:32:09 -0500

    schemairi: switch to http, update to v29.2
    The schema.org site suggests https as canonical, but both being acceptable. Since rdfa-context
    uses http for its schema: prefix, switch to http here to avoid recurring discrepancies.
    
    In the future, maybe include a schemasiri package for https?

--- ontology/schema/schemairi/iri.go
+++ ontology/schema/schemairi/iri.go
@@ -1,4 +1,4 @@
-// see https://schema.org/version/latest/schemaorg-all-https.ttl
+// see https://schema.org/version/latest/schemaorg-all-http.ttl
 package schemairi
 
-//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit irigen -i https://schema.org/version/latest/schemaorg-all-https.ttl -o iri_generated.go
+//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit irigen -i https://schema.org/version/latest/schemaorg-all-http.ttl -o iri_generated.go
