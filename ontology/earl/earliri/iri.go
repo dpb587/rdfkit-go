f08f49cdf196183e18c85bd5b4a27d7b39fe2b43
Danny Berger <dpb587@gmail.com>
2026-01-06 16:51:51 +0000

    ontology: regenerate *iri packages

--- ontology/earl/earliri/iri.go
+++ ontology/earl/earliri/iri.go
@@ -1,3 +1,3 @@
 package earliri
 
-//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit irigen -i https://www.w3.org/ns/earl -o iri_generated.go --in-default-base http://www.w3.org/ns/earl
+//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit export-go-iri -i https://www.w3.org/ns/earl --in-base http://www.w3.org/ns/earl -o iri_generated.go
--- ontology/rdfs/rdfsiri/iri.go
+++ ontology/rdfs/rdfsiri/iri.go
@@ -1,4 +1,4 @@
 // see https://www.w3.org/2000/01/rdf-schema.ttl
 package rdfsiri
 
-//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit irigen -i https://www.w3.org/2000/01/rdf-schema.ttl -o iri_generated.go
+//go:generate go run github.com/dpb587/rdfkit-go/cmd/rdfkit export-go-iri -i https://www.w3.org/2000/01/rdf-schema.ttl -o iri_generated.go
